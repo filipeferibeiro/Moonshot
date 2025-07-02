be8a3952858267f2270a911343488ed96d6b0454
Filipe Fernandes <filipeferibeiro@gmail.com>
2025-07-02 11:54:09 -0300

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Filipe Fernandes on 02/07/25.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
